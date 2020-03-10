1edf21ba84943c45965e9156867eed0a558088f4
maxye <maxye95@gmail.com>
2020-03-09 22:05:35 -0400

    add switch in recordDao

--- code/quizHero_Javalin/src/main/java/dao/Sql2oRecordDao.java
+++ code/quizHero_Javalin/src/main/java/dao/Sql2oRecordDao.java
@@ -1,20 +1,20 @@
-//package dao;
-//
-//import exception.DaoException;
-//import model.Record;
-//import org.sql2o.Connection;
-//import org.sql2o.Sql2o;
-//import org.sql2o.Sql2oException;
-//
-//public class Sql2oRecordDao implements RecordDao {
-//    private Sql2o sql2o;
-//
-//    public Sql2oRecordDao(Sql2o sql2o) {
-//        this.sql2o = sql2o;
-//    }
-//
-//    @Override
-//    public void add(Record record) throws DaoException {
+package dao;
+
+import exception.DaoException;
+import model.Record;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+public class Sql2oRecordDao implements RecordDao {
+    private Sql2o sql2o;
+
+    public Sql2oRecordDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Record record) throws DaoException {
 //        char answer = record.getChoice();
 //
 //        try (Connection conn = sql2o.open()) {
@@ -27,5 +27,32 @@
 //        } catch (Sql2oException ex) {
 //            throw new DaoException("Unable to add record to quiz", ex);
 //        }
-//    }
-//}
+        int fileId = record.getFileId();
+        int questionId = record.getQuestionId();
+        char answer = record.getChoice();
+        String sql = null;
+        switch(answer) {
+            case 'A':
+                sql = "UPDATE Quizzes SET A = A + 1 WHERE fileId = " + fileId + "&& questionId = " + questionId;
+                break;
+            case 'B':
+                sql = "UPDATE Quizzes SET A = B + 1 WHERE fileId = " + fileId + "&& questionId = " + questionId;
+                break;
+            case 'C':
+                sql = "UPDATE Quizzes SET A = C + 1 WHERE fileId = " + fileId + "&& questionId = " + questionId;
+                break;
+            case 'D':
+                sql = "UPDATE Quizzes SET A = D + 1 WHERE fileId = " + fileId + "&& questionId = " + questionId;
+                break;
+        }
+        try (Connection conn = sql2o.open()) {
+            int id = (int) conn.createQuery(sql)
+                    .bind(record)
+                    .executeUpdate()
+                    .getKey();
+            record.setId(id);
+        } catch (Sql2oException ex) {
+            throw new DaoException("Unable to add record to quiz", ex);
+        }
+    }
+}
