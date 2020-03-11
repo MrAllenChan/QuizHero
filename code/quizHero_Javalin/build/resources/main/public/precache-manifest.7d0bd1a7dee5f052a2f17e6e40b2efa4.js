efea19e82ec57af20cf06835cd49bf794b6c6e52
Ziming <zchen85@jhu.edu>
2020-03-11 14:37:01 -0400

    upload resources/public

---
+++ code/quizHero_Javalin/build/resources/main/public/precache-manifest.7d0bd1a7dee5f052a2f17e6e40b2efa4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfa63c8bb1093e4ff39601ca2f3a2da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6937b9f9262143f80017",
+    "url": "/static/css/main.05c03e62.chunk.css"
+  },
+  {
+    "revision": "9d3d08c173ae77000936",
+    "url": "/static/js/2.9346cd98.chunk.js"
+  },
+  {
+    "revision": "47d0732679386f900ca61310a052278d",
+    "url": "/static/js/2.9346cd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6937b9f9262143f80017",
+    "url": "/static/js/main.1bac24e7.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "f45975d0f6312745eb8550b6a3ba8d66",
+    "url": "/static/media/logo.f45975d0.png"
+  }
+]);
