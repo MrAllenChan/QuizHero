c61109fea075194deb007c23012900c2c1f1fde3
yaozixuan <yaozixuan0411@gmail.com>
2020-03-12 14:55:17 -0400

    small fix

--- code/Front End with React/src/components/Result.js
+++ code/Front End with React/src/components/Result.js
@@ -14,7 +14,7 @@ function Result(props) {
             transitionAppearTimeout={500}
         >
             <div>
-                You prefer <strong>{props.quizRquizResultesult}</strong>!
+                Thank you for taking the quiz, your score is <strong>{props.quizResult}</strong>!
             </div>
         </CSSTransitionGroup>
     );
