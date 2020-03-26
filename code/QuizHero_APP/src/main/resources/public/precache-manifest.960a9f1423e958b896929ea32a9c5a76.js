0e7e5dee76fbda29bf0a978388ae66000032d437
yaozixuan <yaozixuan0411@gmail.com>
2020-03-26 14:40:36 -0400

    test 2

---
+++ code/QuizHero_APP/src/main/resources/public/precache-manifest.960a9f1423e958b896929ea32a9c5a76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d576f4a7dd397683d5db2ce81e4da6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a05516da1c18f16c281f",
+    "url": "/static/css/main.bddbb8a0.chunk.css"
+  },
+  {
+    "revision": "892490d4dde99645cb63",
+    "url": "/static/js/2.420284d8.chunk.js"
+  },
+  {
+    "revision": "47d0732679386f900ca61310a052278d",
+    "url": "/static/js/2.420284d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05516da1c18f16c281f",
+    "url": "/static/js/main.3aa5f0c8.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "f45975d0f6312745eb8550b6a3ba8d66",
+    "url": "/static/media/logo.f45975d0.png"
+  }
+]);
