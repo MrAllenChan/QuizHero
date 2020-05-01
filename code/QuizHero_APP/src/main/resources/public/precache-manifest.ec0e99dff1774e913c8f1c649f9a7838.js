6162b8f4947a8381a4abd805ebf7c048c28f80d8
Ziming <zchen85@jhu.edu>
2020-05-01 18:40:35 -0400

    Postman up-to-date resources/public file up-to-date

---
+++ code/QuizHero_APP/src/main/resources/public/precache-manifest.ec0e99dff1774e913c8f1c649f9a7838.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af02c4b52d8da13ed48235f11d65569",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70265524c0ec96d366dc",
+    "url": "./static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "fb0d5830f0240a293700",
+    "url": "./static/css/main.aeed8b4f.chunk.css"
+  },
+  {
+    "revision": "70265524c0ec96d366dc",
+    "url": "./static/js/2.17246708.chunk.js"
+  },
+  {
+    "revision": "0a62f0f44d2f2dd445eb6b2f105e65be",
+    "url": "./static/js/2.17246708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0d5830f0240a293700",
+    "url": "./static/js/main.da996f38.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "./static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "0eb5e1f8967455b4cd9d27ffb2590e5b",
+    "url": "./static/media/logo.0eb5e1f8.png"
+  },
+  {
+    "revision": "cd84ca734d30232a45f085052719d2fb",
+    "url": "./static/media/logo.cd84ca73.png"
+  }
+]);
